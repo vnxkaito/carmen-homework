d7afa29e55ba0e4b2e98bd837998ea010239fadb
vnxkaito <vnxkaito@hotmail.com>
2025-12-26 13:16:45 +0300

    first two answers

--- find_carmen.sql
+++ find_carmen.sql
@@ -1,13 +1,28 @@
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
+select * 
+from public.city as c
+inner join public.country as co
+on c.countrycode = co.code
+where co.region = 'Southern Europe'
+order by c.population asc
+limit 1;
 
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
-
+select cl.* 
+from public.city as c
+inner join public.country as co
+on c.countrycode = co.code
+inner join public.countrylanguage as cl
+on cl.countrycode = co.code
+where co.region = 'Southern Europe'
+order by c.population asc
+limit 1;
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
