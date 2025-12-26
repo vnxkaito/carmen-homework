676df79a9b8a841e96854e8d4a189e7a78444df5
vnxkaito <vnxkaito@hotmail.com>
2025-12-26 13:46:12 +0300

    5th and 6th answer

--- find_carmen.sql
+++ find_carmen.sql
@@ -66,12 +66,26 @@ and c.name != co.name
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
-
+ select co.name, c.name
+from public.country as co
+inner join public.city as c
+on c.countrycode = co.code
+where c.name like 'Serr%'
+and co.region = 'South America'
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
+select * from public.city as cit
+where id = (
+select co.capital
+from public.country as co
+inner join public.city as c
+on c.countrycode = co.code
+where c.name like 'Serr%'
+and co.region = 'South America'
+)
 
 
 
